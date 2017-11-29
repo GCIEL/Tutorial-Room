170bd713b2fb4f1e9651ba9634414d7be29febfe
Zachary Segall <zhhsegall@gmail.com>
2017-11-29 09:39:33 -0600

    Spheres activate only when trigger corresponds to appropriate controller

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -7,10 +7,12 @@ namespace VRTK
     {
         public GameObject roomManager;
         private bool activated = false;
+
         // Use this for initialization
         void Start()
         {
             // Add appropriate error checks
+            // Make abstract class for scene/room manager
             roomManager.GetComponent<LabManager>().rightController.GetComponent<VRTK_ControllerEvents>().TriggerPressed +=
                 new ControllerInteractionEventHandler(Activate);
             roomManager.GetComponent<LabManager>().leftController.GetComponent<VRTK_ControllerEvents>().TriggerPressed +=
@@ -20,11 +22,13 @@ namespace VRTK
         public override void StartUsing(VRTK_InteractUse usingObject)
         {
             base.StartUsing(usingObject);
+            if (!activated) ChangeColor(Color.green);
         }
 
         public override void StopUsing(VRTK_InteractUse usingObject)
         {
             base.StopUsing(usingObject);
+            if(!activated) ChangeColor(Color.red);
         }
 
         protected override void Update()
@@ -35,19 +39,30 @@ namespace VRTK
 
         private void Activate(object sender, ControllerInteractionEventArgs e)
         {
-            if (!activated && IsUsing())
+            if (!activated && IsUsing() &&
+                VRTK_ControllerReference.GetControllerReference(GetUsingObject()).Equals(e.controllerReference))
             {
                 // Update to generealize lab manager object
                 roomManager.GetComponent<LabManager>().inactiveSphereCount = -1;
                 Debug.Log(string.Format("BOOM! {0} spheres left", roomManager.GetComponent<LabManager>().inactiveSphereCount));
 
                 // Change colors
-                Renderer rend = GetComponent<Renderer>();
-                rend.material.shader = Shader.Find("Standard");
-                rend.material.SetColor("_EmissionColor", Color.cyan);
+                ChangeColor(Color.cyan);
 
                 activated = true;
             }
+        }
+
+        private void ChangeColor(Color color)
+        {
+            Renderer rend = GetComponent<Renderer>();
+            rend.material.shader = Shader.Find("Standard");
+            rend.material.SetColor("_EmissionColor", color);
+        }
+
+        protected void DoStartUseObject(object sender, ControllerInteractionEventArgs e)
+        {
+
         }
     }
 }
